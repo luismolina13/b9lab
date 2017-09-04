7a0c0af7b12361c36a0be8c956adf08c2a1de6af
Luis Molina <luismol@umich.edu>
2017-09-04 11:46:39 -0400

    Update Campaign to use modifiers

--- contractFactory/contracts/Campaign.sol
+++ contractFactory/contracts/Campaign.sol
@@ -6,6 +6,7 @@ contract Campaign {
     uint public deadline;
     uint public goal;
     uint public fundsRaised;
+    bool public running;
 
     struct FunderStruct {
         uint amount;
@@ -17,10 +18,21 @@ contract Campaign {
     event LogRefundSent(address funder, uint amount);
     event LogWithdraw(address beneficiary, uint amount);
 
+    modifier onlyIfRunning() {
+        require(running);
+        _;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
     function Campaign(uint duration, uint _goal) {
         owner = msg.sender;
         deadline = block.number + duration;
         goal = _goal;
+        running = true;
     }
 
     function isSuccess() public constant returns (bool isIndeed) {
@@ -31,7 +43,12 @@ contract Campaign {
         return(fundsRaised < goal && block.number > deadline);
     }
 
-    function contribute() public payable returns (bool success) {
+    function runSwitch(bool onOff) public onlyOwner returns (bool success) {
+        running = onOff;
+        return true;
+    }
+
+    function contribute() public onlyIfRunning payable returns (bool success) {
         require(msg.value > 0);
         require(!isSuccess());
         require(!hasFailed());
@@ -43,8 +60,7 @@ contract Campaign {
         return true;
     }
 
-    function withdrawFunds() public returns (bool success) {
-        require(msg.sender == owner);
+    function withdrawFunds() public onlyIfRunning onlyOwner returns (bool success) {
         require(isSuccess());
 
         uint amount = this.balance;
@@ -54,7 +70,7 @@ contract Campaign {
         return true;
     }
 
-    function requestRefund() public returns (bool success) {
+    function requestRefund() public onlyIfRunning returns (bool success) {
         require(hasFailed());
 
         uint amountOwed = funderStructs[msg.sender].amount;
